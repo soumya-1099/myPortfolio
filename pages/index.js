ba990fe891e35e441219e7d12a7cfa797bd1c29a
soumya-1099 <soumyagowda1004@gmail.com>
2023-08-17 00:01:25 +0530

    add: images & schema validation added

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import Herosection from 'components/Herosection'
 import React from 'react'
 import About from 'components/About'
+import Projects from 'components/Projects'
+import Experience from 'components/Experience'
+import Contact from 'components/Contact'
 
 const index = () => {
   return (
     <div>
       <Herosection/>
       <About/>
+      <Projects/>
+      <Experience/>
+      <Contact/>
     </div>
   )
 }
